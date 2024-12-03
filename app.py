128d06c8f6729a5c9ec4209ee49a5b235ae1bb02
Sean Wei <seanwei2001@berkeley.edu>
2024-12-02 22:05:47 -0800

    initial

---
+++ app.py
@@ -0,0 +1,19 @@
+## import cv2
+# import mediapipe as mp
+# import numpy as np
+#
+# import math
+# import tempfile
+# import boto3
+# import json
+# import os
+
+def main():
+    #st.title("Welcome to AceInterview!")
+
+    #st.header("Please upload your recorded interview here:")
+    #data_file = st.file_uploader("Upload Video File", type=["csv"])
+
+
+if __name__ == "__main__":
+    main()
